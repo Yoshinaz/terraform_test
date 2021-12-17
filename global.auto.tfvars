d71b7471643d842d60179d4101ba5f6ee50393a4
Yoshinaz <samanun@honestbank.com>
2021-12-17 13:55:58 +0700

    add checkly code

---
+++ global.auto.tfvars
@@ -0,0 +1,2 @@
+checkly_api_key = "cu_37bad4588f1643a0b79df2b10caa5103"
+checkly_account_id="3514a40d-41c6-48e2-9832-88af822f8db3"
---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    checkly = {
+      source  = "checkly/checkly"
+      version = "~> 1.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "YoshinazOrg"
+
+    workspaces {
+      name = "test"
+    }
+  }
+}
+
+# export TF_VAR_checkly_api_key=""
+variable "checkly_api_key" {
+  description = "Checkly user-level API key - https://app.checklyhq.com/settings/account/api-keys"
+}
+
+# export TF_VAR_checkly_account_id=""
+variable "checkly_account_id" {
+  description = "Checkly Account ID - https://app.checklyhq.com/settings/account/general"
+}
+
+provider "checkly" {
+  api_key    = var.checkly_api_key
+  account_id = var.checkly_account_id
+}
